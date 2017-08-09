e73b7ed8250f8cf910a51187a366b4126bd219bf
Mohamed Ouyizme <mohouyizme@gmail.com>
2017-08-09 19:47:02 +0100

    Watch main.js file in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ gulp.task("serve", ["css"], function() {
     });
     gulp.watch("css/main.scss", ["css"]);
     gulp.watch("index.html").on("change", browserSync.reload);
+    gulp.watch("js/main.js").on("change", browserSync.reload);    
 });
 
 gulp.task("css", function() {
