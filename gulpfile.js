6c305d4a8ed28a13a98aacf4be5ac0ef21892887
Mohamed Ouyizme <mohouyizme@gmail.com>
2017-08-10 16:03:04 +0100

    Minify main.js file

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,17 @@ var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var cssnano = require("cssnano");
+var uglify = require("gulp-uglify");
 var rename = require("gulp-rename");
 var browserSync = require("browser-sync");
 
-gulp.task("serve", ["css"], function() {
+gulp.task("serve", ["css", "js"], function() {
     browserSync.init({
         server: "./"
     });
     gulp.watch("css/main.scss", ["css"]);
     gulp.watch("index.html").on("change", browserSync.reload);
-    gulp.watch("js/main.js").on("change", browserSync.reload);    
+    gulp.watch("js/main.js", ["js"]).on ("change", browserSync.reload);    
 });
 
 gulp.task("css", function() {
@@ -28,5 +29,14 @@ gulp.task("css", function() {
         .pipe(gulp.dest("css"))
         .pipe(browserSync.stream());
 });
+
+gulp.task("js", function() {
+    gulp.src("js/main.js")
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: ".min"
+        }))
+        .pipe(gulp.dest("js"));
+})
 
 gulp.task("default", ["serve"]);
