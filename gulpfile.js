05992e9b9e12faa901fa469a693885034c176600
Mohamed Ouyizme <mohouyizme@gmail.com>
2017-08-14 17:23:17 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,8 @@ gulp.task("serve", ["css", "js"], function() {
     });
     gulp.watch("css/main.scss", ["css"]);
     gulp.watch("index.html").on("change", browserSync.reload);
-    gulp.watch("js/main.min.js", ["js"]).on ("change", browserSync.reload);    
+    gulp.watch("js/main.js", ["js"]);
+    gulp.watch("js/main.min.js").on("change", browserSync.reload);    
 });
 
 gulp.task("css", function() {
